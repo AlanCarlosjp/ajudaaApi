d4ac36b3b2f92aca7dd86c06c4894d9f93685d77
AlanCarlosjp <alandepaulajr@gmail.com>
2023-03-30 08:51:42 -0300

    first commit

---
+++ src/test/java/com/api/ajudaa/ajudaaApi/AjudaaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ajudaa.ajudaaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjudaaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
